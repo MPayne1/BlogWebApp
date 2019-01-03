1411bb8ef7e0f19bc079abf478876bcb7ae00799
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-01-03 16:50:31 +0000

    password strength javascript meter

--- Coursework1/wwwroot/js/site.js
+++ Coursework1/wwwroot/js/site.js
@@ -1,4 +1,31 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript code. 
+
+// using zxcvbn library for password strength
+var strength = {
+    0: "Worst",
+    1: "Bad",
+    2: "Weak",
+    3: "Good",
+    4: "Strong"
+}
+
+var password = document.getElementById('password');
+var strengthMeter = document.getElementById('password-strength');
+var strengthText = document.getElementById('password-strength-text');
+
+
+password.addEventListener('input', function () {
+    var val = password.value;
+    var result = zxcvbn(val);
+
+    strengthMeter.value = result.score;
+
+    if (val !== "") {
+        strengthText.innerHTML = strength[result.score];
+    } else {
+        strengthText.innerHTML = "";
+    }
+});
